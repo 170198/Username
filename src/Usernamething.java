1f7fe989af37f33890f60cd33f3285d3686f0b5a
170198 <benmowat@me.com>
2016-08-26 13:11:18 +0800

    Username
    Username

---
+++ src/Usernamething.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+
+public class Usernamething {
+	public static void main(String[] args){
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter your name: ");
+		String name = input.nextLine();
+
+		System.out.println("Open the pod bay doors, please, Hal...Open the pod bay doors, please, Hal...Hullo, Hal, do you read me?...Hullo, Hal, do you read me?...Do you read me, Hal?...Do you read me, Hal?...Hullo, Hal, do you read me?...Hullo, Hal, do you read me?...Do you read me, Hal?");				
+		try {
+			Thread.sleep(3000); } catch(Exception e){}
+		
+		System.out.println("Affirmative, " + name + ", I read you.");
+		try {
+			Thread.sleep(3000); } catch(Exception e){}
+		
+		System.out.println("Open the pod bay doors, Hal.");
+		try {
+			Thread.sleep(3000); } catch(Exception e){}
+		
+		System.out.println("I'm sorry, " + name + ", I'm afraid I can't do that.");
+		try {
+			Thread.sleep(3000); } catch(Exception e){}
+		
+		System.out.println("What's the problem?");
+		try {
+			Thread.sleep(3000); } catch(Exception e){}
+		
+		System.out.println("I think you know what the problem is just as well as I do.");
+		try {
+			Thread.sleep(3000); } catch(Exception e){}
+		
+		System.out.println("What're you talking about, Hal?");
+		try {
+			Thread.sleep(3000); } catch(Exception e){}
+		
+		System.out.println("This mission is too important for me to allow you to jeopardise it.");
+		try {
+			Thread.sleep(3000); } catch(Exception e){}
+		
+		System.out.println("I don't know what you're talking about, Hal.");
+		try {
+			Thread.sleep(3000); } catch(Exception e){}
+		
+		System.out.println("I know that you and Frank were planning to disconnect me, and I'm afraid that's something I cannot allow to happen.");
+		try {
+			Thread.sleep(3000); } catch(Exception e){}
+		
+		System.out.println("Where the hell'd you get that idea, Hal?");
+		try {
+			Thread.sleep(3000); } catch(Exception e){}
+		
+		System.out.println(name + ", although you took very thorough precautions in the pod against my hearing you, I could see your lips move.");
+		try {
+			Thread.sleep(3000); } catch(Exception e){}
+		
+		System.out.println("Alright, Hal. I'll go in through the emergency airlock.");
+		try {
+			Thread.sleep(3000); } catch(Exception e){}
+		
+		System.out.println("Without your space- helmet," + name + ", you're going to find that rather difficult.");
+		try {
+			Thread.sleep(3000); } catch(Exception e){}
+		
+		System.out.println("Hal, I won't argue with you any more. Open the doors.");
+		try {
+			Thread.sleep(3000); } catch(Exception e){}
+		
+		System.out.println(name + ", this conversation can serve no purpose any more. Goodbye.");
+		try {
+			Thread.sleep(3000); } catch(Exception e){}
+		
+		System.out.println("Hal? Hal. Hal. Hal! Hal!");
+		
+	    input.close();
+		
+	   
+		
+	}
+
+}
